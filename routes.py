b28bd9115a7edad1e1dd966376913fd21ad9d569
radha kapoor <radha@radhakapoor.net>
2014-04-30 16:05:53 -0400

    working sqlite

--- routes.py
+++ routes.py
@@ -30,20 +30,29 @@ def home():
         if value_a and value_b:
             if flask.request.form['calculate'] == 'add':
                 answer = float(value_a) + float(value_b)
+                calculation = Calculation(value_a=value_a, calculation='+', value_b=value_b, answer=answer)
+                db.session.add(calculation)
+                db.session.commit()
                 return 'The answer is '+ '{}'.format(answer)              
             elif flask.request.form['calculate'] == 'subtract':
                 answer = float(value_a) - float(value_b)
+                calculation = Calculation(value_a=value_a, calculation='-', value_b=value_b, answer=answer)
+                db.session.add(calculation)
+                db.session.commit()
                 return 'The answer is ' + '{}'.format(answer)
             elif flask.request.form['calculate'] == 'multiply':
                 answer = float(value_a) * float(value_b)
+                calculation = Calculation(value_a=value_a, calculation='*', value_b=value_b, answer=answer)
+                db.session.add(calculation)
+                db.session.commit()
                 return 'The answer is ' + '{}'.format(answer)
             elif flask.request.form['calculate'] == 'divide':
-                answer = float(value_a) / float(value_b)                 
-                return 'The answer is ' + '{}'.format(answer)              
-                
-            calculation = Calculation(value_a=value_a, calculation=calculation, value_b=value_b, answer=answer)
-            db.session.add(calculation)
-            db.session.commit()                
+                answer = float(value_a) / float(value_b)
+                calculation = Calculation(value_a=value_a, calculation='/', value_b=value_b, answer=answer)
+                db.session.add(calculation)
+                db.session.commit()                 
+                return 'The answer is ' + '{}'.format(answer)         
+                          
         else:
             raise Exception("Please enter two numbers")
             #ValueError does not pick up scenario where user does not select +,-,*,/ or where user enters two non-numbers
@@ -53,7 +62,7 @@ def home():
         return render_template('home.html', calculations=calculations)
 #Is it enough to pass in the variable "calculations or do i also need to pass in value_a, value_b etc?"
  
- #db.session.add(is not picking up the relevant calculation)   
+ 
    
     
     
